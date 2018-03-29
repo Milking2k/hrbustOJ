6eb5fe81c9515b7f9db8eb9f75ed699ff4f532ac
Frank <Milking2k@qq.com>
2018-03-29 23:10:52 +0800

    329upc

--- VJ-18326A.cpp
+++ VJ-18326A.cpp
@@ -5,22 +5,22 @@ using namespace std;
 int ans[45];
 int main(int argc, char const *argv[])
 {
-	int t;
-	ans[0]=0;
-	ans[1]=0;
-	ans[2]=1;
-	ans[3]=2;
-	for (int i = 4; i < 41; ++i)
-	{
-		ans[i] = ans[i-1]+ans[i-2];
-	}
-	cin >> t;
-	while(t--)
-	{
-		int n;
-		cin >> n;
-		printf("%d\n", ans[n]);
-	}
-	
+	// int t;
+	// ans[0]=0;
+	// ans[1]=0;
+	// ans[2]=1;
+	// ans[3]=2;
+	// for (int i = 4; i < 41; ++i)
+	// {
+	// 	ans[i] = ans[i-1]+ans[i-2];
+	// }
+	// cin >> t;
+	// while(t--)
+	// {
+	// 	int n;
+	// 	cin >> n;
+	// 	printf("%d\n", ans[n]);
+	// }
+	cout << (1<<31) << endl;
 	return 0;
 }
