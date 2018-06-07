6b0f442e04f70401ced6fb09efe3c136d4f6976d
Jelech <jelech@hotmail.com>
2018-06-07 21:51:59 +0800

    personal 67

--- f.cpp
+++ f.cpp
@@ -1,31 +1,43 @@
-#include<bits/stdc++.h>
-using namespace std;
-typedef long long ll;
+//#include <bits/stdc++.h>
+#include <iostream>
+#include <cstdio>
+#include<cmath>
+#include<algorithm>
+#include<cstring>
+#include<cstdlib>
 
-ll n;
-string s;
+using namespace std;
+
+
+int T,n,m;
+int emt[100007];
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin >> s;
-    int len = s.size();
-    bool flag = true;
-    int cnt=0;
-    int ans=0;
-    for(int i=0;i<len;i++)
+    scanf("%d",&T);
+    int cont=0;
+    while(cont<T)
     {
-        if(flag && s[i]=='A')
+        cont++;
+        scanf("%d%d",&n,&m);
+        int minans1=0x3f3f3f3f;
+        int minans2=0x3f3f3f3f;
+        int x;
+        for(int i=1;i<=n;i++)
         {
-            cnt=i;
-            flag = false;
+            scanf("%d",&x);
+            minans1 = min(minans1, x);
         }
-        else if(!flag && s[i] == 'Z')
+        for(int i=1;i<=nm;i++)
         {
-            ans = max(ans,i-cnt+1);
+            scanf("%d",&x);
+            minans2 = min(minans2, x);
         }
+        printf("Problem %d:\n",1000+cont);
+        printf("Shortest judge solution: %d bytes.\n", minans1);
+        if(minans2 == 0x3f3f3f3f)
+            printf("Shortest team solution: N/A bytes.\n");
+        else
+            printf("Shortest team solution: %d bytes.\n", minans2);
     }
-    cout << ans << endl;
     return 0;
 }
-
-
