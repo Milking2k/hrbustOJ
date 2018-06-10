4707a8fd051eaebd4163fd3177be89900e7eaede
Milking2k <Milking2k@qq.com>
2018-06-10 23:09:39 +0800

    6-10

---
+++ zoj610A.cpp
@@ -0,0 +1,24 @@
+//#include <bits/stdc++.h>
+#include<iostream>
+#include<cstdio>
+using namespace std;
+typedef long long ll;
+
+const int MAXN = 1e6+7;
+
+int n,m,t;
+int emt[MAXN];
+
+int main()
+{
+    scanf("%d",&t);
+    while(t--)
+    {
+        scanf("%d",&n);
+        for(int i=1;i<=n;i++)
+            printf(i!=n?"0 ":"0\n");
+
+
+    }
+    return 0;
+}
