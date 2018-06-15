0d20a80f615f5fcb9da50dc2494af273feb3819b
Milking2k <Milking2k@qq.com>
2018-06-16 00:00:06 +0800

    11111

---
+++ ZOJ-default.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+const int MAXN = 1e6+7;
+
+int n,m,t;
+int emt[MAXN];
+
+int main()
+{
+    while(~scanf("%d",&n))
+    {
+
+
+    }
+    return 0;
+}
