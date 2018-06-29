f504350b33e3818cbc0c0859ab91c92651f7e916
Milking2k <Milking2k@qq.com>
2018-06-29 22:09:32 +0800

    629

--- cf-492d.cpp
+++ cf-492d.cpp
@@ -1,28 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-struct node
-{
-    int pos,val;
-}emt[207];
-int n;
+int t,n,m;
+int emt[207];
 int vis[207];
-vector<node> s;
 int main()
 {
     scanf("%d",&n);
-    for(int i=1;i<=2*n;i++)
-    {
-        scanf("%d",&emt[i].val);
-        emt[i].pos = i;
+    for(int i=1;i<=2*n;i++){
+        scanf("%d",&emt[i]);
     }
 
-    int ans=0;
-    for(int i=1;i<=2*n;i++)
-    {
-        if(!vis[emt[i].val])
-        {
-
+    int ans = 0;
+    for(int i=1;i<=2*n;i++){
+        if(!vis[i])
+        for(int j=i+1; j<=2*n; j++){
+            if(emt[i] == emt[j]){
+                vis[i] = vis[j] = 1;
+                break;
+            }
+            ans += vis[j]? 0:1;
         }
     }
     printf("%d\n",ans);
