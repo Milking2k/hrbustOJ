9b6f111bf15e02203a6fcfe0c435581948612d6d
Milking2k <Milking2k@qq.com>
2018-10-08 23:35:00 +0800

    cf刷题1030

---
+++ Codeforces/cf-1030-B.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,a,n) for (int i = (a); i<(n); i++)
+#define per(i,a,n) for (int i = (n)-1; i>=(a); i--)
+#define pb push_back
+#define mp make_pair
+#define all(now) (now).begin(),(now).end()
+#define fi first
+#define se second
+#define SZ(now) ((int)(now).size())
+typedef vector<int> VI;
+typedef long long ll;
+typedef pair<int,int> PII;
+const ll mod = 1e9+7;
+inline int read(int &n){char c=getchar();int x=0,f=1;while(c<'0'||c>'9'){if(c=='-')f=-1;c=getchar();}while(c>='0'&&c<='9'){x=x*10+(c-'0');c=getchar();}return n = x*f;}
+inline void read(int &a,int &b){read(a),read(b);}
+inline void read(int &a,int &b,int &c){read(a,b),read(c);};
+ll powmod(ll a,ll b) {ll res = 1; a%=mod; assert(b>=0); for(;b; b>>=1){if(b&1)res = res*a%mod; a = a*a%mod;}return res;}
+ll gcd(ll a,ll b) { return b?gcd(b,a%b):a;}
+// head
+
+int T,n, m, k;
+ll a, b;
+
+bool isPrime(ll k)
+{
+    for (ll i = 2; i * i <= k;i++){
+        if(k%i==0){
+            return false;
+        }
+    }
+    return true;
+}
+int main()
+{
+    for (scanf("%d", &T); T;T--){
+        scanf("%lld%lld", &a, &b);
+        if(a-b!=1){
+            printf("NO\n");
+        }
+        else{
+            printf(isPrime(a + b)?"YES\n":"NO\n");
+        }
+    }
+        return 0;
+}
